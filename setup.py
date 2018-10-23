f4adfad7bf9a9927c0dce9151605c576e2454b9a
growlot <lovemobileweb227@aol.com>
2018-10-23 03:30:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ with open('requirements-dev.txt') as f:
     tests_requires = [line.strip() for line in f if line.strip()]
 
 setup(
-    name='erc20token',
+    name='erc20token-python',
     version=__version__,
     description='ERC20 token SDK for Python',
-    author='Kin Foundation',
-    author_email='david.bolshoy@kik.com',
-    maintainer='David Bolshoy',
-    maintainer_email='david.bolshoy@kik.com',
-    url='https://github.com/kinfoundation/erc20token-sdk-python',
+    author='imperchik',
+    author_email='imperchik@gmail.com',
+    maintainer='imperchik',
+    maintainer_email='imperchik@gmail.com',
+    url='https://github.com/growlot/erc20token-sdk-python',
     license='GPLv2',
-    packages=["erc20token"],
+    packages=["erc20token-python"],
     long_description=open("README.md").read(),
     keywords=["ethereum", "erc20", "blockchain", "cryptocurrency"],
     classifiers=[
@@ -28,10 +28,9 @@ setup(
         'Development Status :: 4 - Beta',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
     install_requires=requires,
     tests_require=tests_requires,
-    python_requires='>=2.7',
+    python_requires='>=3.6.6',
 )
